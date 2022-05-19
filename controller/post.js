076238d90804a08ef68a50ecbc56a85180ea95e1
huang-hua1212 <hahahazzz123@gmail.com>
2022-05-19 23:05:15 +0800

    update controller/post

--- controller/post.js
+++ controller/post.js
@@ -1,7 +1,8 @@
 var multer = require('multer');
+const postModel = require('../models/post');
 
 const posts = {
-    getAll: (req, res) => {
+    getAll: async (req, res) => {
         postModel.find().limit(50).populate({
             path: 'user',
             select: 'name photo'
@@ -17,6 +18,56 @@ const posts = {
                 status: 'success',
                 datas,
             });
+        });
+    },
+    getPostByPostId: (req, res) => {
+        const id = req.params.id;
+
+        postModel.findById(id).populate({
+            path: 'user',
+            select: 'name photo'
+        }).populate({
+            path: 'commentDetail',
+            select: 'user content likes whoLikes createdAt updatedAt',
+            populate: {
+                path: 'user',
+                select: 'name photo'
+            }
+        }).exec(function (err, datas) {
+            if (datas) {
+                res.status(200).json({
+                    status: 'success',
+                    datas,
+                });
+            } else {
+                return next(appError(400, "欄位未填寫正確", next));
+            }
+        });
+    },
+    getPostByUserId: (req, res) => {
+        const id = req.params.id;
+
+        postModel.find({
+            user: id
+        }).populate({
+            path: 'user',
+            select: 'name photo'
+        }).populate({
+            path: 'commentDetail',
+            select: 'user content likes whoLikes createdAt updatedAt',
+            populate: {
+                path: 'user',
+                select: 'name photo'
+            }
+        }).exec(function (err, datas) {
+            if (datas) {
+                res.status(200).json({
+                    status: 'success',
+                    datas,
+                });
+            } else {
+                return next(appError(400, "欄位未填寫正確", next));
+            }
         });
     }
 }
