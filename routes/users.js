cf247e82a3f27b5a47e04a5428a3dcb1084569f9
huang-hua1212 <hahahazzz123@gmail.com>
2022-05-25 19:30:57 +0800

    update new week5_hw-heroku.postman

--- routes/users.js
+++ routes/users.js
@@ -20,7 +20,6 @@ var uploadMulter = multer({
 })
 
 
-
 /* GET users BY id */
 router.get('/:id', (req, res, next) => {
   usersController.getUserByUserId(req, res, next);
@@ -36,41 +35,6 @@ router.patch('/:id', (req, res, next) => {
 /* GET users listing. with FormData image */
 router.patch('/with-FormDataImage/:id', uploadMulter.single('photo'), refreshToken, uploadImg, (req, res, next) =>{
   postsController.patchUserWithFormDataImg(req, res, next);
-  // const obj = req.body;
-  // const keys_1 = Object.keys(obj);
-  // const properties = ['name', 'username', 'password', 'role', 'sex', 'photo', 'followings', 'tokens'];
-  // obj.photo = req.imgFile.link;
-  // var resObj = obj;
-  // const id = req.params.id;
-  // userModel.findByIdAndUpdate(id, resObj)
-  //   .then((result) => {
-  //     var fail = 0;
-  //     if (!result) {
-  //       throw new Error(false);
-  //     }
-  //     keys_1.forEach((value) => {
-  //       console.log(value);
-  //       if (properties.indexOf(value) === -1) {
-  //         fail += 1;
-  //       }
-  //       resObj[value] = obj[value];
-  //     })
-  //     if (fail > 0) {
-  //       res.status(400).json({ status: 'false', message: "欄位未填寫正確，或無此 ID" });
-
-  //     } else {
-  //       res.status(200).json({
-  //         status: 'success',
-  //         data: '更新成功',
-  //       })
-  //     }
-  //   })
-  //   .catch(() => {
-  //     res.status(400).json({
-  //       status: 'false',
-  //       data: '更新失敗或無此ID',
-  //     })
-  //   });
 });
 
 module.exports = router;
