aa7a3745da81fe687bd795bcdaa163b8457b36e6
huang-hua1212 <hahahazzz123@gmail.com>
2022-06-08 11:33:05 +0800

    update sessionLogin/sign_in

--- routes/sessionLogin.js
+++ routes/sessionLogin.js
@@ -3,6 +3,8 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/sign_in', (req, res, next) => {
+
+   console.log(req);
    if (req.body.firstName == "" || req.body.lastName == "") {
       res.json({ result: 'firstName和lastName其一為空!!!' });
       //  return res.redirect('Login.html');
@@ -12,14 +14,12 @@ router.post('/sign_in', (req, res, next) => {
    {
       req.session.time++; //同一連線的登入次數, 就加 1
       res.json({ result: 'session已存在!!!連線次數加一' });
-      //   return res.redirect('/session');    //就直接導向到...
    }
    else {
       //session store裡沒有的，就會重新設置
       req.session.firstName = req.body.firstName;
       req.session.lastName = req.body.lastName;
       req.session.time = 1;
-      res.header("Access-Control-Allow-Credentials", true);
       //    return res.redirect('/session');
       res.json({ result: 'session未存在，新增一個新的session!!!' });
    }
@@ -29,7 +29,6 @@ router.post('/sign_in', (req, res, next) => {
 
 // 進入需要驗證的頁面...
 router.get('/verify', function (req, res) {
-   // ...
    var name = 'guest';
    var isLogin = false;
    var Logtime = 1;
